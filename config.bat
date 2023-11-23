6ce17b8810518c8796c6519edc29dc493d5d46a4
TipherethA <TA@noexist.io>
2023-11-23 15:35:30 +0800

    move hp widget

--- config.bat
+++ config.bat
@@ -1,6 +1,6 @@
 set UE4PATH=D:\prog\Epic Games\UE4\UE_4.26
-set GAMEPATH=D:\work\EnderLiliesMod\ENDER LILIES\EnderLilies
-set GAMEPATH2=D:\work\executor\ENDER LILIES\EnderLilies
+set GAMEPATH=D:\EnderLiliesMod\Games\DebugMod\ENDER LILIES\EnderLilies
+set GAMEPATH2=D:\EnderLiliesMod\Games\ScriptMod\ENDER LILIES\EnderLilies
 set MODNAME=DebugMod
 set MOD2NAME=ExtraMod
 
